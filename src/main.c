cd686eb81aabcf00307af83d6aa80c90168e4429
MatheusCSimoes <simoes.matheusc@gmail.com>
2020-11-08 12:47:58 -0300

    Inserindo operações de I/O e colocando randomização na criação dos processos e I/Os

--- src/main.c
+++ src/main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h> //apenas para utilizar rand() para gerar tempos dos processos
 
 #include "queue.h"
 #include "process.h"
 #include "iodevice.h"
 
-#define Nprocess 5
-#define SliceTime 3
-#define IOdisco 5
-#define IOfita 10
-#define IOimpressora 15
+#define Nprocess 5 //numero de processos para simulacao
+#define SliceTime 3 //tamanho da fatia de tempo para execucao no algoritmo RR
+#define IOdisco 5 //tempo para I/O em disco
+#define IOfita 10 //tempo para I/O em fita magnetica
+#define IOimpressora 15 //tempo para I/O em impressora
 
 Queue *high;
 Queue *low;
@@ -19,6 +20,10 @@ IOdevice *impressora;
 
 Process *processes[Nprocess];
 
+int currentTime = 0;
+int remainingSliceTime = SliceTime;
+int currentProcessId = -1;
+
 void init() {
   high = createQueue(Nprocess);
   low = createQueue(Nprocess);
@@ -27,9 +32,14 @@ void init() {
   fitaMagnetica = createIOdevice(IOfita, Nprocess);
   impressora = createIOdevice(IOimpressora, Nprocess);
 
+  printf("Criando processos:\n");
   for(int i = 0; i < Nprocess; i++) {
-    processes[i] = createProcess(i, 5, 10*i); //id, tempo de exec, tempo de chegada
+    int execTime = (rand() % 20) + 5;
+    int startTime = (rand() % 50);
+    processes[i] = createProcess(i, execTime, startTime); //id, tempo de exec, tempo de chegada
+    printf("Processo %d: tempo de inicio = %d, tempo de execucao = %d.\n", i, startTime, execTime);
   }
+  printf("\n");
 }
 
 void checkNewProcess(int currentTime) { //se novos processos iniciarem, coloca-los na fila
@@ -41,50 +51,97 @@ void checkNewProcess(int currentTime) { //se novos processos iniciarem, coloca-l
   }
 }
 
+void getNewProcessId() {
+  if(currentProcessId == -1) {
+    if(!isEmpty(high)) {
+      currentProcessId = dequeue(high);
+    }
+    else {
+      currentProcessId = dequeue(low);
+    }
+
+    remainingSliceTime = SliceTime;
+
+    if(currentProcessId >= 0) {
+      printf("t = %d: Processo %d retirado da fila e entra em execucao\n", currentTime, currentProcessId);
+    }
+  }
+}
+
 void checkIOdevices() {
-    //verifica andamento das operacoes de IO
+  int requestIO = (rand() % 100) > 90; //90% de chance de um processo pedir I/O em cada instante de tempo
+  if(requestIO && currentProcessId >= 0) {
+    int IOtype = (rand() % 3); //33% de chance do I/O ser de cada tipo
+
+    switch (IOtype) {
+      case 0 /*impressora*/:
+        enqueueIO(impressora, currentProcessId);
+        printf("t = %d: Processo %d inserido na fila de I/O para impressora.\n", currentTime, currentProcessId);
+        break;
+      case 1 /*fita*/:
+        enqueueIO(fitaMagnetica, currentProcessId);
+        printf("t = %d: Processo %d inserido na fila de I/O para fita magnetica.\n", currentTime, currentProcessId);
+        break;
+      case 2 /*disco*/:
+        enqueueIO(disco, currentProcessId);
+        printf("t = %d: Processo %d inserido na fila de I/O para disco.\n", currentTime, currentProcessId);
+        break;
+    }
+
+    currentProcessId = -1;
+    getNewProcessId(); //pega novo processo para executar enquanto o atual é passado para IO
+  }
 }
 
 void runScheduler() {
   int processToExec = Nprocess;
-  int currentTime = 0;
-  int remainingSliceTime = SliceTime;
-  int currentProcessId = -1;
+
+  currentTime = 0;
+  remainingSliceTime = SliceTime;
+  currentProcessId = -1;
 
   while(processToExec > 0) {
     checkNewProcess(currentTime); //verifica se algum novo processo foi iniciado
 
-    if(currentProcessId == -1) {
-      if(!isEmpty(high)) {
-        currentProcessId = dequeue(high);
-      }
-      else {
-        currentProcessId = dequeue(low);
-      }
+    getNewProcessId();
 
-      remainingSliceTime = SliceTime;
-
-      if(currentProcessId >= 0) {
-        printf("t = %d: Processo %d retirado da fila e entra em execucao\n", currentTime, currentProcessId);
-      }
-    }
+    checkIOdevices(); //verifica andamento das operacoes de IO
 
     currentTime = currentTime + 1;
 
-    if(currentProcessId < 0) {
+    //execucao dos I/Os
+    int pidDisco = execIO(disco, 1);
+    if(pidDisco >= 0) {
+      printf("t = %d: Processo %d terminou I/O em disco e é inserido na fila de baixa prioridade.\n", currentTime, pidDisco);
+      enqueue(low, pidDisco);
+    }
+
+    int pidFita = execIO(fitaMagnetica, 1);
+    if(pidFita >= 0) {
+      printf("t = %d: Processo %d terminou I/O em fita magnetica e é inserido na fila de alta prioridade.\n", currentTime, pidFita);
+      enqueue(high, pidFita);
+    }
+
+    int pidImpressora = execIO(impressora, 1);
+    if(pidImpressora >= 0) {
+      printf("t = %d: Processo %d terminou I/O em impressora e é inserido na fila de alta prioridade.\n", currentTime, pidImpressora);
+      enqueue(high, pidImpressora);
+    }
+
+    if(currentProcessId < 0) { //verifica se tem algum processo para ser executado pela CPU
       printf("t = %d: CPU ociosa.\n", currentTime);
       continue;
     }
 
     exec(processes[currentProcessId], 1); //executa o processo
 
-    if(remaining(processes[currentProcessId]) == 0) {
+    if(remaining(processes[currentProcessId]) == 0) { //verifica se o processo terminou execucao completa
       printf("t = %d: O processo %d finalizou sua execucao\n", currentTime, currentProcessId);
       processToExec--;
       currentProcessId = -1;
     }
     else {
-      if(remainingSliceTime - 1 == 0) {
+      if(remainingSliceTime - 1 == 0) { //verifica se a fatia de tempo para o processo acabou
         enqueue(low, currentProcessId);
         printf("t = %d: Processo %d foi inserido na fila de baixa prioridade\n", currentTime, currentProcessId);
         currentProcessId = -1;
@@ -93,8 +150,6 @@ void runScheduler() {
         remainingSliceTime = remainingSliceTime - 1;
       }
     }
-
-    checkIOdevices(); //verifica andamento das operacoes de IO
   }
 }
 
